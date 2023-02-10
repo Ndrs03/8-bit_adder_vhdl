83277e07b9c16f9f6aa080fafc3c6111a6485169
Ndrs03 <kaa.alexandersson@outlook.com>
2023-02-10 13:24:41 +0100

    samma fast utan grnerate loopen

--- add8.vhdl
+++ add8.vhdl
@@ -20,8 +20,21 @@ ARCHITECTURE struct OF add8 IS
 BEGIN
     carry(0) <= cin;
     sum(0) <= a(0) XOR b(0) XOR carry(0);
-    T_LABEL : FOR i IN 1 TO 7 GENERATE
-        u1 : fa PORT MAP(a(i), b(i), carry(i - 1), sum(i), carry(i));
-    END GENERATE;
+    
+    u1 : fa PORT MAP(a(1), b(1), carry(0), sum(1), carry(1));
+    u2 : fa PORT MAP(a(2), b(2), carry(1), sum(2), carry(2));
+    u3 : fa PORT MAP(a(3), b(3), carry(2), sum(3), carry(3));
+    u4 : fa PORT MAP(a(4), b(4), carry(3), sum(4), carry(4));
+    u5 : fa PORT MAP(a(5), b(5), carry(4), sum(5), carry(5));
+    u6 : fa PORT MAP(a(6), b(6), carry(5), sum(6), carry(6));
+    u7 : fa PORT MAP(a(7), b(7), carry(6), sum(7), carry(7));
+
+
+    --T_LABEL : FOR i IN 1 TO 7 GENERATE
+      --  u1 : fa PORT MAP(a(i), b(i), carry(i - 1), sum(i), carry(i));
+    --END GENERATE;
+
+
     cout <= carry(7);
 END struct;
+
